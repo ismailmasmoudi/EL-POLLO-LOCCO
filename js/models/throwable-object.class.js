7d97330e62d0abeacb37ac6b53db2c83faa73cd1
Ismail Masmoudi <155138711+ismailmasmoudi@users.noreply.github.com>
2024-11-12 16:19:56 +0100

    fix throwableobjects

--- js/models/throwable-object.class.js
+++ js/models/throwable-object.class.js
@@ -1,52 +1,51 @@
 class ThrowableObject extends MovableObject {
-    /**
-     * Indicates whether the bottle has collided.
-     * @type {boolean}
-     */
+
     bottleCollided = false;
-    /**
-     * Indicates whether the bottle is currently spinning.
-     * @type {boolean}
-     */
     isSpinning = false;
 
     IMAGES_BOTTLE_SPIN = [
-        './img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
-        './img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
-        './img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
-        './img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
+        'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
     ];
 
-  
     IMAGE_BOTTLE_SPLASH = [
-        './img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
-        './img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
-        './img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
-        './img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
-        './img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
-        './img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png',
     ];
 
     /**
-     * Constructs a Bottle object with specified coordinates and direction.
-     * @param {number} x - The initial x-coordinate of the bottle.
-     * @param {number} y - The initial y-coordinate of the bottle.
-     * @param {boolean} isAhead - Whether the bottle is thrown ahead (true) or behind (false).
+     * Constructs a Bottle object with specified coordinates.
+     * Loads the image assets for the bottle and its animations.
+     * Sets the initial position and dimensions of the bottle.
+     * Initiates the throwing animation and the animation loop.
+     * @param {number} x - The x-coordinate of the bottle's position.
+     * @param {number} y - The y-coordinate of the bottle's position.
      */
     constructor(x, y, isAhead) {
-        super().loadImage('./img/6_salsa_bottle/salsa_bottle.png');
+        super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
         this.loadImages(this.IMAGES_BOTTLE_SPIN);
         this.loadImages(this.IMAGE_BOTTLE_SPLASH);
         this.x = x;
         this.y = y;
         this.ahead = isAhead;
-        this.offset = { top: 0, left: 0, right: 0, bottom: 0 };
-        this.throw();
+        this.offset = {
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0
+        }
+        this.throw(100, 150);
         this.animate();
     }
 
     /**
-     * Marks the bottle as collided, stops its vertical movement, and plays a sound effect.
+     * Marks the bottle as collided and plays a sound effect.
      */
     bottleIsColliding() {
         this.bottleCollided = true;
@@ -57,14 +56,20 @@ class ThrowableObject extends MovableObject {
     }
 
     /**
-     * Initiates the throwing animation and movement of the bottle.
+     * Initiates the throwing action of the bottle.
+     * Sets the initial vertical speed, starts spinning, plays a sound effect, and applies gravity.
+     * Additionally, it sets an interval to move the bottle horizontally.
      */
     throw() {
         this.speedY = 20;
         this.isSpinning = true;
         this.applyGravity();
         setInterval(() => {
-            this.ahead ? this.x += 10 : this.x -= 10;
+            if (this.ahead === true) {
+                this.x += 10;
+            } else {
+                this.x -= 10;
+            }
         }, 25);
         if (soundManager.isSoundOn) {
             soundManager.bottleThrowSound.play();
@@ -72,28 +77,40 @@ class ThrowableObject extends MovableObject {
     }
 
     /**
-     * Animates the bottle, switching between spinning and splash animations.
-     */
-    animate() {
-        let splashAnimationStarted = false;
+       * Animates the spinning of the bottle while it's in the air.
+       */
+    animateSpin() {
         setInterval(() => {
             if (this.isSpinning) {
                 this.playAnimation(this.IMAGES_BOTTLE_SPIN);
-            } else if (this.bottleCollided && !splashAnimationStarted) {
-                this.playSplashAnimation();
-                splashAnimationStarted = true;
             }
         }, 50);
     }
 
     /**
-     * Plays the bottle splash animation and removes the bottle from the game after a delay.
+     * Plays the splash animation once and removes the bottle after a delay.
      */
-    playSplashAnimation() {
-        this.isSpinning = false;
-        this.playAnimation(this.IMAGE_BOTTLE_SPLASH);
-        setTimeout(() => {
-            this.removeFromGame();
-        }, this.IMAGE_BOTTLE_SPLASH.length * 100);
+    animateSplash() {
+        let splashAnimationStarted = false;
+
+        setInterval(() => {
+            if (this.bottleCollided && !splashAnimationStarted) {
+                this.isSpinning = false;
+                this.playAnimation(this.IMAGE_BOTTLE_SPLASH);
+                splashAnimationStarted = true;
+                setTimeout(() => {
+                    this.removeFromGame();
+                }, this.IMAGE_BOTTLE_SPLASH.length * 100);
+            }
+        }, 100);
+    }
+
+    /**
+     * Initiates the animation of the bottle.
+     * Starts both the spinning and splash animation loops.
+     */
+    animate() {
+        this.animateSpin();
+        this.animateSplash();
     }
 }
